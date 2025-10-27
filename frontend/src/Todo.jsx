3eb65e26b55584a14908da427659e44636a7962c
abc <abc@gmail.com>
2025-10-27 23:29:58 +0530

    New update

--- frontend/src/Todo.jsx
+++ frontend/src/Todo.jsx
@@ -12,7 +12,7 @@ export default function Todo() {
     const [editTitle, setEditTitle] = useState("");
     const [editDescription, setEditDescription] = useState("");
 
-    const apiUrl = "http://localhost:8000"
+    const apiUrl = "https://todo-app-server-five-lovat.vercel.app"
     
     const handleSubmit = () => {
         setError("")
